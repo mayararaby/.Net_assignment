863291ee0c62632c717719be17b372e6bbc58f3e
mayararaby <mayararaby246@gmail.com>
2021-04-21 15:42:34 -0700

    A1

---
+++ A_1/A_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            int hour = 23;
+            if (hour >= 0 && hour < 24)
+                Console.WriteLine("True");
+            else
+                hour = 0;
+            Console.WriteLine("Enter the hour");
+            int hours = int.Parse(Console.ReadLine());
+            do
+            { 
+                if (hours >= 1 && hours <=23)
+                {
+                    break;
+                }
+                Console.WriteLine("Enter the hour");
+                hours = int.Parse(Console.ReadLine());
+            } while ( hours>=
+            0);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ExerciseThree/ExerciseThree/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseThree
+{
+    enum BankingTypes
+    {
+        checking = 1,
+        Deposit = 2
+    }
+    struct BankAccount
+    {
+        public long AccountNumber;
+        public decimal AccountBalance;
+        public BankingTypes AccountType;
+        public void Print()
+        {
+            Console.WriteLine(
+                $"Account Number {this.AccountNumber}, " +
+                $"Account Type {this.AccountType}, " +
+                $"Account Balance = {this.AccountBalance}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount firstInstance;
+            Console.WriteLine("Enter Account Balance ");
+            firstInstance.AccountBalance = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Enter Account Number ");
+            firstInstance.AccountNumber = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Number Of Account Type");
+            Console.WriteLine($"1- checking \n" +
+                $"2- Deposit "
+               );
+            int option = int.Parse((Console.ReadLine()));
+            if (option == 1)
+            {
+                firstInstance.AccountType = BankingTypes.checking;
+            }
+            else if (option == 2)
+            {
+                firstInstance.AccountType = BankingTypes.Deposit;
+            }
+            firstInstance.Print(); 
+            Console.ReadLine();
+        }
+    }
+}
