bcac7b8865cc95d2567f27d72d1afc553865c2f8
mayararaby <mayararaby246@gmail.com>
2021-04-21 06:46:38 -0700

    the cecond

---
+++ ExerciseTwo/ExerciseTwo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseTwo
+{
+    enum BankingTypes
+    {
+        checking = 1,
+        Deposit = 2
+    }
+    struct BankAccount
+    {
+        public long AccountNumber;
+        public decimal AccountBalance;
+        public BankingTypes AccountType;
+
+        public override string ToString()
+        {
+            return $"Account Balance = {AccountBalance}, Account Number =" +
+             $"{AccountNumber}, Account Type = {AccountType}";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (int i in Enum.GetValues(typeof(BankingTypes)))
+            {
+                Console.WriteLine($" {i}");
+            }
+            BankAccount firstInstance;
+            firstInstance.AccountBalance = 2000;
+            firstInstance.AccountNumber = 12374645198;
+            firstInstance.AccountType = BankingTypes.checking;
+            Console.WriteLine(firstInstance.ToString());
+            Console.ReadLine();
+        }
+    }
+}
