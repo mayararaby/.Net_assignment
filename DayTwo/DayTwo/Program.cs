5721850af07e79cb36633f84d4b454c92c826615
mayararaby <mayararaby246@gmail.com>
2021-04-21 06:12:48 -0700

    task one done

---
+++ DayTwo/DayTwo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayTwo
+{
+    enum BankingTypes 
+    {
+        checking = 1,
+        Deposit = 2
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (int i in Enum.GetValues(typeof(BankingTypes)))
+            {
+                Console.WriteLine($" {i}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
